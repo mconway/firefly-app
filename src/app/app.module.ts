16543516642c3d1e565dd6e760aa90960cc2cfd8
Mike Conway <mike.conway@wiredbyte.com>
2018-04-23 00:00:56 -0700

    delete about page (unneeded)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
@@ -32,7 +31,6 @@ import { AppVersion } from '@ionic-native/app-version'
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
     ContactPage,
     HomePage,
     TabsPage,
@@ -52,7 +50,6 @@ import { AppVersion } from '@ionic-native/app-version'
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
     ContactPage,
     HomePage,
     TabsPage,
