958f6a09c387f52a4c24a271764bdb15894fc69d
Mike Conway <mike.conway@wiredbyte.com>
2018-09-24 18:46:53 -0700

    #13 fix styling of goal progress bars #9 Update icon for Piggy Banks

--- src/pages/menu/menu.ts
+++ src/pages/menu/menu.ts
@@ -21,7 +21,7 @@ export class MenuPage{
         { title: 'Overview', component: HomePage, icon: "home", index: 1 },
         { title: 'Accounts', component: AccountsPage, icon: "card", index: 0 },
         { title: 'Bills', component: BillsPage, icon: "calendar", index: 2 },
-        { title: 'Piggy Banks', component: PiggyBanksPage, icon: "wallet", index: 2 },
+        { title: 'Piggy Banks', component: PiggyBanksPage, icon: "list", index: 2 },
         { title: 'Transactions', component: TransactionsPage, icon: "refresh", index: 0 },
         { title: 'Settings', component: SettingsPage, icon: "settings", index: 3 },
     ];
